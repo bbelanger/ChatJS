a707434bf4b8a471b0cc38901b2dc24ab77001fc
André Rodrigues Pena <andrerpena@gmail.com>
2014-05-30 23:04:08 -0300

    Fixing the ChatJS directory

---
+++ ChatJs/js/jquery.chatjs.adapter.ts
@@ -0,0 +1,59 @@
+﻿// Documentation: http://www.chatjs.net/Docs/AboutTheAdapters.html
+// represents the communication channel between ChatJS and the server
+interface IAdapter {
+    // called when the adapter is initialized
+    init: (done: () => void) => void;
+
+    // functions called by the server, to contact the client
+    client: IClientAdapter;
+
+    // functions called by the client, to contact the server
+    server: IServerAdapter;
+}
+
+// functions called by the server, to contact the client
+interface IClientAdapter {
+
+    // called by the server when a new message has arrived
+    onMessagesChanged(handler: (message: ChatMessageInfo) => void): void;
+
+    // called by the server when a user is typing
+    onTypingSignalReceived(handler: (typingSignal: ChatTypingSignalInfo) => void): void;
+
+    // called by the server when the user list changed
+    onUserListChanged(handler: (userListData: ChatUserListChangedInfo) => void): void;
+
+    // called by the server when the room list changed
+    onRoomListChanged(handler: (roomListData: ChatRoomListChangedInfo) => void): void;
+}
+
+// functions called by the client, to contact the server
+interface IServerAdapter {
+
+    // sends a message to a room, conversation or user
+    sendMessage(roomId: number, conversationId: number, otherUserId: number, messageText: string, clientGuid: string, done: () => void): void;
+
+    // sends a typing signal to a room, conversation or user
+    sendTypingSignal(roomId: number, conversationId: number, userToId: number, done: () => void): void;
+
+    // gets the message history from a room, conversation or user
+    getMessageHistory(roomId: number, conversationId: number, otherUserId: number, done: (messages: Array<ChatMessageInfo>) => void): void;
+
+    // gets the given user info
+    getUserInfo(userId: number, done: (userInfo: ChatUserInfo) => void): void;
+
+    // gets the user list in a room or conversation
+    getUserList(roomId: number, conversationId: number, done: (userList: Array<ChatUserInfo>) => void): void;
+
+    // gets the room list
+    getRoomsList(done: (roomsList: Array<ChatRoomInfo>) => void): void;
+
+    // enters the given room
+    enterRoom(roomId: number, done: () => void): void;
+
+    // leaves the given room
+    leaveRoom(roomId: number, done: () => void): void;
+}
+
+
+
---
+++ ChatJs/js/jquery.chatjs.window.js
@@ -0,0 +1,135 @@
+﻿var ChatWindowOptions = (function () {
+    function ChatWindowOptions() {
+    }
+    return ChatWindowOptions;
+})();
+
+var ChatWindow = (function () {
+    function ChatWindow(options) {
+        var _this = this;
+        var defaultOptions = new ChatWindowOptions();
+        defaultOptions.isMaximized = true;
+        defaultOptions.canExpand = true;
+        defaultOptions.canClose = true;
+        defaultOptions.onCreated = function () {
+        };
+        defaultOptions.onClose = function () {
+        };
+        defaultOptions.onMaximizedStateChanged = function () {
+        };
+
+        this.options = $.extend({}, defaultOptions, options);
+
+        if (this.options.canExpand) {
+            this.$windowTray = $("<div/>").addClass("chat-window-tray").appendTo($("body"));
+            this.$windowTray.on("click", function () {
+                _this.toggleMaximizedState();
+            });
+        }
+
+        // window
+        this.$window = $("<div/>").addClass("chat-window").appendTo($("body"));
+        if (this.options.canExpand)
+            this.$window.addClass("expansible");
+        if (this.options.width)
+            this.$window.css("width", this.options.width);
+        if (this.options.height)
+            this.$window.css("height", this.options.height);
+
+        // title
+        this.$windowTitle = $("<div/>").addClass("chat-window-title").appendTo(this.$window);
+        if (this.options.canClose) {
+            var $closeButton = $("<div/>").addClass("close").appendTo(this.$windowTitle);
+            $closeButton.click(function (e) {
+                e.stopPropagation();
+
+                // removes the window
+                _this.$window.remove();
+
+                // triggers the event
+                _this.options.onClose(_this);
+            });
+        }
+        $("<div/>").addClass("text").text(this.options.title).appendTo(this.$windowTitle);
+
+        // content
+        this.$windowContent = $("<div/>").addClass("chat-window-content").appendTo(this.$window);
+        this.$windowInnerContent = $("<div/>").addClass("chat-window-inner-content").appendTo(this.$windowContent);
+
+        // wire everything up
+        this.$windowTitle.click(function () {
+            _this.toggleMaximizedState();
+        });
+
+        this.setMaximized(this.options.isMaximized, false);
+
+        this.options.onCreated(this);
+    }
+    ChatWindow.prototype.getWidth = function () {
+        return this.options.canExpand ? this.$windowTray.outerWidth() : this.$window.outerWidth();
+    };
+
+    ChatWindow.prototype.setRightOffset = function (offset) {
+        this.$window.css("right", offset);
+        if (this.options.canExpand)
+            this.$windowTray.css("right", offset);
+    };
+
+    ChatWindow.prototype.setTitle = function (title) {
+        $("div[class=text]", this.$windowTitle).text(title);
+    };
+
+    ChatWindow.prototype.setVisible = function (visible) {
+        if (visible)
+            this.$window.show();
+        else
+            this.$window.hide();
+    };
+
+    // returns whether the window is maximized
+    ChatWindow.prototype.isMaximized = function () {
+        return !this.$window.hasClass("minimized");
+    };
+
+    ChatWindow.prototype.setMaximized = function (isMaximized, triggerMaximizedStateEvent) {
+        if (typeof triggerMaximizedStateEvent === "undefined") { triggerMaximizedStateEvent = true; }
+        // windows are maximized if the this.$windowContent is visible
+        if (!this.options.canExpand) {
+            if (isMaximized) {
+                // if it can't expand and is maximized
+                this.$window.removeClass("minimized");
+                this.$windowContent.show();
+            } else {
+                // if it can't expand and is minimized
+                this.$window.addClass("minimized");
+                this.$windowContent.hide();
+            }
+        } else {
+            if (isMaximized) {
+                // if it can expand and is maximized
+                this.$window.show();
+                this.$window.removeClass("minimized");
+                this.$windowTray.removeClass("minimized");
+            } else {
+                // if it can't expand and is minimized
+                this.$window.hide();
+                this.$window.addClass("minimized");
+                this.$windowTray.addClass("minimized");
+            }
+        }
+        if (triggerMaximizedStateEvent)
+            this.options.onMaximizedStateChanged(this, isMaximized);
+    };
+
+    ChatWindow.prototype.toggleMaximizedState = function () {
+        this.setMaximized(this.$window.hasClass("minimized"));
+    };
+    return ChatWindow;
+})();
+
+// The actual plugin
+$.chatWindow = function (options) {
+    var chatWindow = new ChatWindow(options);
+    return chatWindow;
+};
+//# sourceMappingURL=jquery.chatjs.window.js.map
